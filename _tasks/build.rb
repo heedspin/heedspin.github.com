1c891f53d4b354fbae42d6ab239677f9f49280d0
Tim Harrison <tim.harrison@yahoo.com>
2010-12-18 17:30:19 -0800

    Blog be born.

---
+++ _tasks/build.rb
@@ -0,0 +1,34 @@
+# Rakefile
+require 'rake'
+ 
+# tasks/site.rake
+namespace :site do
+  desc "Build css files from .less files"
+  task :less do
+    less_directory = '_less'
+    css_directory = 'css'
+    Dir.glob(less_directory + '/*.less').each do |file|
+      if File.file?(file)
+        file_name = file.split('/').last.split('.').first
+        puts "* Creating css file for less file named #{file_name} \n"
+        `lessc #{less_directory}/#{file_name}.less #{css_directory}/#{file_name}.css`
+      end
+    end
+  end
+  
+  desc "Build static files from jekyll files"
+  task :jekyll do
+    puts "* Building static files from jekyll files"
+    `jekyll`
+  end
+  
+  desc "Build site (less and jekyll)"
+  task :build do
+    puts "[START] Building site [START]"
+    Rake::Task["site:less"].invoke
+    Rake::Task["site:jekyll"].invoke
+    puts "[END] Building site [END]"
+  end
+end
+
+task :default => 'site:build'
